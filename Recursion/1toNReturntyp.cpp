ba6fe154c58bcf09f7bfceb91fdaa482d26e36e7
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-15 16:50:40 +0530

    Recursion

--- Recursion/1toNReturntyp.cpp
+++ Recursion/1toNReturntyp.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int sum ( int n) {
     // cout<<n<< " ";
     if ( n==1) return 1;
-    return n + sum (n -1);   
+    return n + sum (n -1);   //step I = n ==1 X then return 5 + sum (4);  then 5 + 10 = 15 then hi return main fun value 
+                            //step II = n == 1X the return 4 + sum (3); then 4 + 6 = 10 value of this call is 10 ^ 
+                            //step III = n == 1X then return 3 + sum(2); then 3 + 3 = 6 value of this call is 6 ^
+                            //step IV = n == 1 X then return 2 + sum(1);   2 + 1 = 3 value of this call is 3 ^
+                            //step V = n == 1 Yes then hi pass value for IV call then ^
 }
 int main () {
    int n ;
--- Recursion/factRec.cpp
+++ Recursion/factRec.cpp
@@ -1,29 +1,13 @@
-// #include <iostream>
-// using namespace std;
-//  int fact( int n) {
-//  if ( n == 0 || n == 1) return 1;
-//  return n * fact( n -1);
-// }
-// int main ( ){
-//     int n ;
-//     cout<<"Enter the N number :";
-//     cin>>n;
-//     cout<<fact(n);
-// }
-
-// Make a function which claculate 'a' raised to teh power 'b' using recursion 
 #include <iostream>
 using namespace std;
-int pow ( int a , int b){
-    if(  b ==0) return 1;
-    return a * pow( a , b -1);
+ int fact( int n) {
+ if ( n == 0 || n == 1) return 1;
+ return n * fact( n -1);
 }
-int main() {
-    int a ,b ;
-    cout<<" Enter the A number :";
-    cin>>a;
-    cout<<" Enter the B number :";
-    cin >>b;
-    cout<<pow( a, b);
+int main ( ){
+    int n ;
+    cout<<"Enter the N number :";
+    cin>>n;
+    cout<<fact(n);
+}
 
-}
