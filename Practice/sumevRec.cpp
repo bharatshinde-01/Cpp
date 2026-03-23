62ed0d21ed9011f13d3d9c681de2051ff2b2831d
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-23 19:16:15 +0530

    Count Even number

--- Practice/sumevRec.cpp
+++ Practice/sumevRec.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 using namespace std;
     int sumev(int n) {
-        if( n == 0) return 0;
-        int d = n %10;
-        if( d % 2 == 0)  // change only == to != then print odd numbers sum
-        return d +sumev(n /10);
-        else
-        return sumev(n /10);
+        // if( n == 0) return 0;
+        // int d = n %10;
+        // if( d % 2 == 0)  // change only == to != then print odd numbers sum
+        // return d +sumev(n /10);
+        // else
+        // return sumev(n /10);
+        if ( n ==0 ) return 0;
+        int d = n % 10;
+        if( d % 2 == 0) {
+            return 1 + sumev( n / 10);
+        }
+        else return sumev( n / 10);
     }
 int main() {
     int n ;
