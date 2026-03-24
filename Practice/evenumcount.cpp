6c3738da319b34de91d93ff8860bfbe899c11c61
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-24 08:33:21 +0530

    Fix error

--- Practice/evenumcount.cpp
+++ Practice/evenumcount.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-
+    int count(int n) {
+        if( n == 0) return 0;
+        int d = n % 10;
+        if( d % 2 == 0) return 1 + count(n / 10);
+        else 
+        return count ( n / 10);
+    }
 int main() {
-    
+    int n ;
+    cout << "Enter the n number :";
+    cin >> n;
+    cout << "The count of even numbers is :"<<count( n );
     return 0;
 }
