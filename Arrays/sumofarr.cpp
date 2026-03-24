7e531b5d71e6b0ae1319126334c12b26837eea52
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-24 09:21:00 +0530

    Sum of array elements

--- Arrays/sumofarr.cpp
+++ Arrays/sumofarr.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 
 int main() {
-    
+    int arr[5]= { 1, 2, 3,4,5};
+    int n = sizeof(arr)/4;
+    cout<<"Your array numbers is this :";
+    for (int i = 0; i < n; i++) {
+        cout<<arr[i]<<"  ";
+    }
+    int sum = 0, i;     // for product of array simple change 0 to 1 and + to * 
+    for( i = 0; i <=4 ;  i++){
+      sum = sum + arr[i];
+    }
+    cout<<endl;
+    cout << "The sum of array is :"<<sum;
+
     return 0;
 }
