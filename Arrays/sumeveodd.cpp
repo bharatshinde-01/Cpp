83c615f0e8e4cd1c6a19ba39a5082653dc25dc07
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-27 11:25:43 +0530

    Add coments

--- Arrays/sumeveodd.cpp
+++ Arrays/sumeveodd.cpp
@@ -17,7 +17,7 @@ int main() {
     }
 
     for (int i = 0; i < n; i++) {
-        if( i % 2 ==0){
+        if( i % 2 ==0){         // i is a index of array and arr[i] is element of an array 
             eve = eve + arr[i];
         }
         else {
