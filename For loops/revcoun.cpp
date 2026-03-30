4fd9274770d21c32f68ac6e5f0b7fbac4c182d43
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-30 21:31:43 +0530

    Recerce number printing

---
+++ For loops/revcoun.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the n number u want to reverse counte :";
+    cin >> n;
+    for (int i = n; i >=1; i--) {
+        cout << "  "<<i;
+    }
+    return 0;
+}
