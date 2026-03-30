546498e7f81a4f9728f564e5cc4f971dfa83f02b
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-03-30 17:36:47 +0530

    Add syntax and comments and Add element & remove element on array

--- Vector/syntax.cpp
+++ Vector/syntax.cpp
@@ -6,16 +6,20 @@ int main() {
     int n = 5;
     // cout << "Enter the Vector array size :";
     // cin >> n;
-    vector<int> arr(n);
+    vector<int> arr(n); // Vector madhe aapn default value pan deu shkato EX. arr(n,4) atta 4 print honar a
+    // ani by defaut valur 0 aste  
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     for (int i = 0; i < n; i++) {
         cout << "  "<<arr[i];
     }
-    cout << endl <<"Before add element of size of vector arrya is :"<<arr.size();
-        arr.push_back(10);
-    cout << endl <<" Size of the vactor array after add element "<<arr.size()<<endl;
+    cout << endl <<"Before add element of size of vector arrya is :"<<arr.size(); // Array chi size kiti aahe te dakhvto 
+
+        arr.push_back(10); // Array madhe last la element add karto 
+
+    cout << endl <<" Size of the vactor array after add element "<<arr.size()<<endl;// Array chi size ek element add jalya vr kiti ahe te dakhvto 
+
     cout << "After add ele print array";
     for (int i = 0; i < n; i++) {
         cout << " "<<arr[i];
