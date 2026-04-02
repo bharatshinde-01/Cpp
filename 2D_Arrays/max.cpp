ccb0d11bd1ab6c910793e77fc3c6cad3a9229449
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-04-02 10:51:31 +0530

    Max ans Min element's of 2d array

--- 2D_Arrays/max.cpp
+++ 2D_Arrays/max.cpp
@@ -2,28 +2,37 @@
 using namespace std;
 
 int main() {
-    int arr[3][4];
+    int arr[2][3];
     cout << "Enter the array element's :";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
             cin>>arr[i][j];
         }
     }
     cout << "Your array elements is :"<<endl;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
             cout<<arr[i][j]<<"  ";
         }
         cout<<endl;
     }
     int max = 0;
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
             if( arr[i][j] > max){
                 max = arr[i][j];
             }
         }
     }
-    cout << "Your max element is this :"<<max;
+    int min = max;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            if(arr[i][j] < min) {
+                min = arr[i][j];
+            }
+        }
+    }
+    cout << "Your max element is this :"<<max<<endl;
+    cout << "Your min element is this :"<<min;
     return 0;
 }
