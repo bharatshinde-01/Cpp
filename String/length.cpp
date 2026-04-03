831cad31afbe8ab110b83dcd1845ea568d48ecf1
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-04-03 16:23:03 +0530

    Use builtin function to calculate giver string

---
+++ String/length.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str = "Bharat";
+    // cout << str.size()<<endl;  //hi String chi lenth print kartat
+    // cout << str.length();
+    int n = str.length(); // n var madhe str chi size store hoti 
+    for (int i = 0; i < str.length(); i++) {// ithe n ni pan apan print karu shakto
+        cout <<str[i];
+    }
+        //Printing String using for loop
+    return 0;
+}
