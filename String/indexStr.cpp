c4b5f835606d10ee2d3dee87168edb8f28aeb6ef
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-04-03 11:19:55 +0530

    Update string using index

---
+++ String/indexStr.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str = "Bharat";
+    cout << "your string"<<endl<<str;
+    str[0] = 's'; // shting have index just like a array's
+    str[5] = 'D';// you can update string value's and other opetactions
+    cout << endl<<"updated string"<<endl<<str;
+    return 0;
+}
