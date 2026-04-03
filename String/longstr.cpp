f9795f5ff1456f1692aa188cf66b17c20d384cf9
bharatshinde-eng <bharatshinde7054@gmail.com>
2026-04-03 11:20:28 +0530

    Learn with spaces input using getline

---
+++ String/longstr.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str ;
+    cout << "Enter the string:";
+    getline(cin , str);
+    cout << "Your string is this :"<<str;
+
+    return 0;
+}
