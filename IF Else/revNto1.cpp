ab851a95a0c6f2c212973c54fb2ffdd8f9012c24
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-04 16:59:02 +0530

    Reverse N to 1 using if else or for loop

---
+++ IF Else/revNto1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n ;
+    cout << "Enter the n number :";
+    cin >> n;
+    for (int i = n; i >= 1; i--) {
+        if( i <= n){
+            cout << i<<"  ";
+    }
+    }
+    return 0;
+}
