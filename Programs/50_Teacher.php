65d79b00765db44e1e613f76b2dd3d908eb7f5a2
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-07 20:49:48 +0530

    . Using laravel create a page showing the teacher details (name, contact no, subject taught). Add Navigation on the home page called

---
+++ Programs/50_Teacher.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+
+<!-- Navigation -->
+<div style="background-color:lightgray; padding:10px;">
+    <a href="/">Home</a> |
+    <a href="/teacher">Teacher Details</a>   <!-- navigation link -->
+</div>
+
+<h2>Welcome to College Website</h2>
+
+</body>
+</html>
