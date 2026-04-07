c810fb830a1d30c1753826dffbadf0a90065db4a
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-07 20:44:55 +0530

    . Using laravel create a module containing details of your college. On the home page add Navigation which contains your

---
+++ Programs/48_Collage.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>College</title>
+</head>
+<body>
+
+<!-- Navigation Bar -->
+<div style="background-color:lightblue; padding:10px;">
+    <img src="logo.png" width="50" height="50">   <!-- logo image -->
+    <b>My College Name</b>   <!-- college name -->
+</div>
+
+<h2>College Details</h2>
+
+<p>College Name: ABC College</p>
+<p>Address: Mumbai</p>
+<p>Courses: BBA, BCA, MCA</p>
+<p>Contact: 1234567890</p>
+
+</body>
+</html>
