1543f07782e5126ac0c1cf10727f6863a6d8eac9
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-17 11:00:46 +0530

    What is class and object with Syntax & example

---
+++ Class/syntax.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student {         // user definded data type 
+                    // Class is User definided data type that contains of data member or
+                    // member  is a templet or bluprint 
+    public:
+    string name;
+    int rollno;
+};
+int main() {
+    Student s1; // s1 is a object of a class 
+                // Object of instance of a class that which contains actual values of data members 
+    cout<<"Enter the name :";;
+
+    getline(cin,s1.name);
+    cout<<"Enter roll no :";
+    cin>>s1.rollno;             // s1.rollno is called Data member access 
+    cout<<s1.name << "  " <<endl <<s1.rollno;
+    return 0;
+}
