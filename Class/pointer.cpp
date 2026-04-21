c3a56c3f42aa85451f8f017aa02da3a3ce3032f2
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-21 14:52:52 +0530

    Pointer object

--- Class/pointer.cpp
+++ Class/pointer.cpp
@@ -16,13 +16,17 @@ public:
     }
 };
     void change(Collage* c) {
-        (*c).name="mc";
+        (*c).name="mc"; // use for (*c) or c->
     }
 int main() {
     Collage c("Chutiya", 500 , 12);
-    Collage * p1(&c);
-    p1-> name = "gandu";
+    Collage * p1(&c); // Store object address 
+                      // If u can store add then u can access all if u wnat to change any thisng
+   
+    p1-> name = "gandu"; // -> or (*p1);
+
     c.display();
+
     change(&c);
     c.display();
     return 0;
