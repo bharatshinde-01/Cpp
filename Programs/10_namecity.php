11b02abf2392190df14d8535fe923bf75ace7fae
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-04-23 22:21:56 +0530

    Write PHP program accept name, select your cities you would like to visit and display

--- Programs/10_namecity.php
+++ Programs/10_namecity.php
@@ -2,32 +2,29 @@
 <body>
 
 <form method="post">
-Name: <input type="text" name="name"><br><br>
+    Name: <input type="text" name="name"><br><br>
 
-Select Cities:<br>
-<input type="checkbox" name="city[]" value="Mumbai">Mumbai<br>
-<input type="checkbox" name="city[]" value="Pune">Pune<br>
-<input type="checkbox" name="city[]" value="Delhi">Delhi<br>
-<input type="checkbox" name="city[]" value="Goa">Goa<br><br>
+    Select Cities:<br>
+    <input type="checkbox" name="city[]" value="Pune"> Pune<br>
+    <input type="checkbox" name="city[]" value="Mumbai"> Mumbai<br>
+    <input type="checkbox" name="city[]" value="Delhi"> Delhi<br>
+    <input type="checkbox" name="city[]" value="Goa"> Goa<br><br>
 
-<input type="submit" value="Submit">
+    <input type="submit" value="Submit">
 </form>
 
 <?php
-
-if(isset($_POST['name']))
-{
+if(isset($_POST['name']) && isset($_POST['city'])){
     $name = $_POST['name'];
+    $cities = $_POST['city'];   // multi-valued parameter (array)
 
-    echo "Name: ".$name."<br>";
-    echo "Selected Cities:<br>";
+    echo "Name: $name <br>";
+    echo "Cities selected:<br>";
 
-    foreach($_POST['city'] as $c)   // multi-valued parameter (array)
-    {
-        echo $c."<br>";
+    foreach($cities as $c){
+        echo $c . "<br>";
     }
 }
-
 ?>
 
 </body>
