bf60313559fc30c4e697a696a02950ee4ee21e5a
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-05-03 22:06:17 +0530

    Sorted array

---
+++ Arrays/Sort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter array size :";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements :";
+    for (int i = 0; i < n; i++) {
+        cin>>arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j =i+ 1; j < n; j++) {
+            if( arr[i]> arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    cout << "Sorted array :";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] <<"  " ;
+    }
+    return 0;
+}
