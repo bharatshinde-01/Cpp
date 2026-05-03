12897d0e3e4a637eac403409568f4ec82fbfda33
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-05-03 17:46:36 +0530

    Create file using file

---
+++ Files/Create.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ofstream f;
+    f.open("test.txt");
+    f << "Mera pehla f program!";
+    f.close();
+    cout << "Hello";
+    return 0;
+}
