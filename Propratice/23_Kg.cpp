60293dc638600baf2925afd764e02d2c0e1e1730
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-05-12 20:19:58 +0530

    kilo in to gram and gram into kilo

--- Propratice/23_Kg.cpp
+++ Propratice/23_Kg.cpp
@@ -1,2 +1,36 @@
-Create a base class Conversion. Derive class Weight (Gram, Kilogram) from Conversion
-class. Write a C++ program to read, convert and display operations.
+// Create a base class Conversion. Derive class Weight (Gram, Kilogram) from Conversion
+// class. Write a C++ program to read, convert and display operations.
+#include <bits/stdc++.h>
+using namespace std;
+    class Conversion {
+    public:
+        float gm , kg;
+        Conversion(){}
+        void Get() {
+            cout << "Enter Gram :";
+            cin >> gm;
+            cout << "Enter kilo gram :";
+            cin >> kg;
+        }
+    };
+    class Weight : public  Conversion {
+    public:
+    float kilo , gram;
+        Weight(){}
+        float Gram() {
+            gram =  gm / 1000;
+            cout << "Gram into kilo gram :"<<gram<<endl;
+            
+        }
+        float Kilo(){
+            gram = kg * 1000;
+            cout << "Kilo into gram :"<<gram;
+        }
+    };
+int main() {
+    Weight w;
+    w.Get();
+    w.Gram();
+    w.Kilo();
+    return 0;
+}
