8d3604432e744f6af2948318ec937e2ca64a8789
bharatshinde-01 <bharatshinde7054@gmail.com>
2026-06-02 10:47:47 +0530

    inline function

---
+++ inline/areain.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+inline float Area (float r = 5){
+    cout<<"Area of circle is :" << 3.14* r * r <<endl;
+    cout<<"Circumference  of circle is :" << 2 * 3.14 * r ;
+    return 0;
+}
+int main() {
+    cout <<Area();
+    return 0;
+}
